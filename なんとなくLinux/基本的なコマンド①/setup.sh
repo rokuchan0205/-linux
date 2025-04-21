071a5bcab0dc839ac01be4b9f35bca9775144809
rokuchan0205 <163642754+rokuchan0205@users.noreply.github.com>
2025-04-21 17:00:51 +0900

    Add files via upload

---
+++ なんとなくLinux/基本的なコマンド①/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 既存の異常なファイルを削除
+rm -f example.txt test.txt test_dir .hidden
+
+# ls コマンド用（step1）
+touch test.txt
+touch example.txt
+mkdir test_dir
+touch test_dir/sample.txt
+touch .hidden
