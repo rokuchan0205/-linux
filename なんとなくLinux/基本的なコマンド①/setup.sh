228d1831aba0320f0173720332469198fe31579a
rokuchan0205 <163642754+rokuchan0205@users.noreply.github.com>
2025-04-21 16:49:53 +0900

    Update setup.sh

--- なんとなくLinux/基本的なコマンド①/setup.sh
+++ なんとなくLinux/基本的なコマンド①/setup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# クリーンアップ（念のため）
-find /root -type f -name $'*\r' -delete
-find /root -type d -name $'*\r' -exec rm -rf {} \;
-
-# ls, cp, パーミッション用（step1, step3, step4）
 touch test.txt
 touch example.txt
 mkdir test_dir
